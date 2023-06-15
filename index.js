b08afc2db22b72e81c2bea2d1b7b1cee7316fffb
Kashish Fatima <kashishfatima253@gmail.com>
2023-06-15 22:25:23 +0500

    first commit

--- index.js
+++ index.js
@@ -24,6 +24,17 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.get("/api/whoami", function (req, res) {
+  // console.log(res)
+  var ip = process.env.IP_ADDRESS;
+  var lang = req.headers["accept-language"];
+  var software = req.headers["user-agent"];
+  res.json({
+    "ipaddress": ip,
+   "language": lang, 
+   "software":software});
+});
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
